64289de25fd7a49345439270747b1ad993a8f3a0
integeek <ambre.giraudlanza@outlook.fr>
2026-05-18 15:33:47 +0200

    Connexion à la base de donnée

---
+++ Model/Bdd.php
@@ -0,0 +1,34 @@
+<?php
+try {
+    $db = new PDO(
+        "pgsql:host=aws-0-eu-west-1.pooler.supabase.com;port=6543;dbname=postgres",
+        "postgres.mrevazltwkhjmjyhxboa",
+        "",
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+    );
+} catch (Exception $e) {
+    die("Erreur de connexion à la base de données : " . $e->getMessage());
+}
+echo "Connexion réussie !";
+?>
+
+
+<?php
+class Bdd {
+    private static $instance = null;
+    public static function getInstance() {
+        if (self::$instance === null) {
+            try {
+                $db = new PDO(
+                    "pgsql:host=aws-0-eu-west-1.pooler.supabase.com;port=6543;dbname=postgres",
+                    "postgres.mrevazltwkhjmjyhxboa",
+                    "",
+                    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+                );
+            }  catch (Exception $e) {
+                die("Erreur de connexion à la base de données : " . $e->getMessage());
+            }
+        }
+        return self::$instance;
+    }
+}
